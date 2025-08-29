50cdbb575566fed8196a2b0e0077281a7dc89904
Arcxelix <arcxelix@gmail.com>
2025-08-29 11:53:49 +0530

    Fix: add Android Gradle Plugin + Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    // No plugins at root
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
 
 allprojects {
